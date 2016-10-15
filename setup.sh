d0112364ab41896e91423703b45d95149cbd7aef
Shin Yamamoto <shin@scenee.com>
2016-10-16 08:46:20 +0900

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,13 @@
+#!/bin/bash
+
 ln -s ${PWD}/.vimrc ~/.vimrc
-
-if test -f ~/.gitconfig;
-then
-	ln -s ${PWD}/.gitconfig ~/.gitconfig
-else
-	echo "Warning: Found a existing .gitconfig: $HOME/.gitconfig"
-	echo "	Please set up .gitconfig manually"
-fi
-
 ln -s ${PWD}/.git-completion.bash ~/.git-completion.bash
 ln -s ${PWD}/.tigrc ~/.tigrc
+ln -s ${PWD}/.gitconfig ~/.gitconfig
 
-mkdir ~/.vim/bundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Install vundle
+if ! test -d ~/.vim/bundle;
+then
+	mkdir ~/.vim/bundle
+	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
