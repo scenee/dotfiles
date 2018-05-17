3c7be631f1ab62db9d04a7922b509fd176c90a6b
Shin Yamamoto <shin@scenee.com>
2018-05-17 18:16:35 +0900

    Remove install_vundle.sh

--- Makefile
+++ Makefile
@@ -10,10 +10,6 @@ test:
 setup: clean
 	./setup.sh
 
-.PHONY: install_vundle
-install_vundle:
-	@./install_vundle.sh
-
 .PHONY: install_plug
 install_plug:
 	@./install_vim-plug.sh
--- install_vundle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if test -d ~/.vim/bundle;
-then
-	echo "Already installed: ~/.vim/vundle"
-else
-	echo "Install Vundle for vim..."
-	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
