2602a1a372d33fb66654346ad41597251b779c07
Shin Yamamoto <shin@scenee.com>
2019-06-28 11:52:03 +0900

    modify clean.sh

--- clean.sh
+++ clean.sh
@@ -3,15 +3,17 @@
 echo "Removing symbolic links to dotfiles..."
 
 pushd ~/ >/dev/null
-[[ -L .bash_profile ]] && rm -f .bash_profile
+[[ -L .profile ]] && rm -f .profile
+[[ -L .bashrc ]] && rm -f .bashrc
 [[ -L .vimrc ]] && rm -f .vimrc
 [[ -L .git-completion.bash ]] && rm -f .git-completion.bash
 [[ -L .tigrc ]] && rm -f .tigrc
 [[ -L .gitconfig ]] && rm -f .gitconfig
+[[ -L .tmux.conf ]] && rm -f .tmux.conf
 [[ -L .plantuml ]] && rm -rf .plantuml
 [[ -L .ripgreprc ]] && rm -rf .ripgreprc
 popd >/dev/null
 
 echo "Cleaning gems..."
 
-gem uninstall -I -a -x --user-install --force
+gem uninstall -I -a -x --user-install
