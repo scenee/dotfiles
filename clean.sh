1579d98c7001b104c07c8cfb23e12bb9a981aebf
Shin Yamamoto <shin@scenee.com>
2019-08-19 17:02:50 +0900

    git: add gitignore_global

--- clean.sh
+++ clean.sh
@@ -3,12 +3,14 @@
 echo "Removing symbolic links to dotfiles..."
 
 pushd ~/ >/dev/null
-[[ -L .profile ]] && rm -f .profile
 [[ -L .bashrc ]] && rm -f .bashrc
+[[ -L .zprofile ]] && rm -f .zprofile
+[[ -L .profile ]] && rm -f .profile
 [[ -L .vimrc ]] && rm -f .vimrc
-[[ -L .git-completion.bash ]] && rm -f .git-completion.bash
 [[ -L .tigrc ]] && rm -f .tigrc
 [[ -L .gitconfig ]] && rm -f .gitconfig
+[[ -L .gitignore_global ]] && rm -f .gitignore_global
+[[ -L .git-completion.bash ]] && rm -f .git-completion.bash
 [[ -L .tmux.conf ]] && rm -f .tmux.conf
 [[ -L .plantuml ]] && rm -rf .plantuml
 [[ -L .ripgreprc ]] && rm -rf .ripgreprc
--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ ln -s ${DOTFILES}/.zprofile
 ln -s ${DOTFILES}/.profile
 ln -s ${DOTFILES}/.vimrc
 ln -s ${DOTFILES}/.git-completion.bash
+ln -s ${DOTFILES}/.gitignore_global
 ln -s ${DOTFILES}/.tigrc
 ln -s ${DOTFILES}/.gitconfig
 ln -s ${DOTFILES}/.tmux.conf
