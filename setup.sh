7b8fbdcdd5d549ac5709e2676ed69d1e613a2447
Shin Yamamoto <shin@scenee.com>
2021-01-14 09:42:46 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,10 +8,10 @@ ln -s ${DOTFILES}/.zshrc
 ln -s ${DOTFILES}/.zprofile
 ln -s ${DOTFILES}/.profile
 ln -s ${DOTFILES}/.vimrc
-ln -s ${DOTFILES}/git-completion.bash git-completion.bash
-ln -s ${DOTFILES}/.gitignore_global
-ln -s ${DOTFILES}/.tigrc
 ln -s ${DOTFILES}/.gitconfig
+ln -s ${DOTFILES}/.gitignore_global
+ln -s ${DOTFILES}/git-completion.bash git-completion.bash
+ln -s ${DOTFILES}/.tigrc
 ln -s ${DOTFILES}/.tmux.conf
 ln -s ${DOTFILES}/.plantuml
 ln -s ${DOTFILES}/.ripgreprc
