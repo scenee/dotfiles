c61f4f980a5794b3fc6dbb1d4fa1eaa1dd16bfe1
Shin Yamamoto <shin@scenee.com>
2022-01-04 23:32:03 +0900

    fish: modify the path config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,16 @@
+# Aliases
+
 alias mux="tmuxinator"
 alias studio="open -a /Applications/Android\ Studio.app"
 alias getplain="pbpaste | pbcopy"
 
-# Bind
+# Binds
+
 bind \cd delete-char # Stop an unexpected exit
+
+# Paths
+
+set -x PATH /usr/local/opt/python@3.9/libexec/bin $PATH
+set -x PATH /usr/local/opt/opencv@3/bin $PATH
+set -x PATH $HOME/.slack/bin $PATH
+set -x PATH $HOME/.deno/bin $PATH
