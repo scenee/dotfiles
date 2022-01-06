0468a7d2565d02479c460422aa42f739bcaf21e2
Shin Yamamoto <shin@scenee.com>
2022-01-06 14:45:12 +0900

    fish: fix PATH env for macports in tmux

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,10 @@ bind \cd delete-char # Stop an unexpected exit
 
 # Paths
 
+if test -n $TMUX
+set -x PATH /opt/local/bin /opt/local/sbin $PATH
+end
+
 set -x PATH /usr/local/opt/python@3.9/libexec/bin $PATH
 set -x PATH /usr/local/opt/opencv@3/bin $PATH
 set -x PATH $HOME/.slack/bin $PATH
