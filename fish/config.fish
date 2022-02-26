067bebb9051728d6ae99992e92a36bcddbb0e160
Shin Yamamoto <shin@scenee.com>
2022-02-26 12:42:06 +0900

    sh: start using asdf again for flutter

--- fish/config.fish
+++ fish/config.fish
@@ -12,3 +12,10 @@ bind \cd delete-char # Stop an unexpected exit
 if test -n $TMUX
 set -x PATH /opt/local/bin /opt/local/sbin $PATH
 end
+
+# asdf
+
+set -x ASDF_CONFIG_FILE ~/.config/asdf
+set -x ASDF_DIR ~/.local/asdf
+set -x ASDF_DATA_DIR ~/.local/asdf
+source ~/.local/asdf/asdf.fish
