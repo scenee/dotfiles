614c4c1002ed7a5d73a6c1600b9dcfcfe43d4424
Shin Yamamoto <shin@scenee.com>
2022-02-28 16:07:29 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,18 +7,18 @@ pushd ~/ >/dev/null
 mkdir -p .local/go
 mkdir -p .local/cargo
 
-ln -shf ${DOTFILES}/.shinit
-ln -shf ${DOTFILES}/.bashrc
-ln -shf ${DOTFILES}/.zshrc
-ln -shf ${DOTFILES}/.zprofile
-ln -shf ${DOTFILES}/.profile
-ln -shf ${DOTFILES}/.vimrc
-ln -shf ${DOTFILES}/.gitconfig
-ln -shf ${DOTFILES}/.gitignore_global
-ln -shf ${DOTFILES}/git-completion.bash .git-completion.bash
-ln -shf ${DOTFILES}/.tigrc
-ln -shf ${DOTFILES}/.tmux.conf
-ln -shf ${DOTFILES}/.plantuml
-ln -shf ${DOTFILES}/.ripgreprc
-ln -shf ${DOTFILES}/fish .config/fish
+ln -snf ${DOTFILES}/.shinit
+ln -snf ${DOTFILES}/.bashrc
+ln -snf ${DOTFILES}/.zshrc
+ln -snf ${DOTFILES}/.zprofile
+ln -snf ${DOTFILES}/.profile
+ln -snf ${DOTFILES}/.vimrc
+ln -snf ${DOTFILES}/.gitconfig
+ln -snf ${DOTFILES}/.gitignore_global
+ln -snf ${DOTFILES}/git-completion.bash .git-completion.bash
+ln -snf ${DOTFILES}/.tigrc
+ln -snf ${DOTFILES}/.tmux.conf
+ln -snf ${DOTFILES}/.plantuml
+ln -snf ${DOTFILES}/.ripgreprc
+ln -snf ${DOTFILES}/fish .config/fish
 
