50a449e796b41d4adc0c8c9a01d85b33a27170cb
Shin Yamamoto <shin@scenee.com>
2022-12-19 10:08:22 +0900

    git: update git-completion.bash

--- update_git-completion.sh
+++ update_git-completion.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -o ./.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o ./git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
