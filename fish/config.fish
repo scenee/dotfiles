82c46495c7f698d9b876b96a6f34e944853cf7d1
Shin Yamamoto <shin@scenee.com>
2023-05-29 09:23:34 +0900

    fish: modify tmuxp aliases

--- fish/config.fish
+++ fish/config.fish
@@ -32,9 +32,9 @@ end
 
 # tmuxp
 
-alias mux='tmuxp'
-alias fmux='tmuxp ls | fzf --layout=reverse --info=inline --height=40% --print0 | xargs -0 -o tmuxp load'
-alias kmux='tmux list-sessions | cut -d" " -f1 | cut -d":" -f1 | fzf --layout reverse --info=inline --height=40% | xargs tmux kill-session -t'
+alias mx='tmuxp'
+alias fmx='tmuxp ls | fzf --layout=reverse --info=inline --height=40% --print0 | xargs -0 -o tmuxp load'
+alias kmx='tmux list-sessions | cut -d" " -f1 | cut -d":" -f1 | fzf --layout reverse --info=inline --height=40% | xargs tmux kill-session -t'
 
 # pnpm
 
