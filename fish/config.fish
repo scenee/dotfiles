7c0e7b8ded9a8a6f2f247d88b15de7af9d3d0889
Shin Yamamoto <shin@scenee.com>
2024-09-27 14:36:32 +0900

    fish: add android path

--- fish/config.fish
+++ fish/config.fish
@@ -33,3 +33,9 @@ fish_add_path "$HOME/.pub-cache/bin"
 # asdf: This is a workaround for PATH in tmux
 
 fish_add_path "$HOME/.local/asdf/shims"
+
+
+# android
+
+fish_add_path $HOME/Library/Android/sdk/platform-tools
+
