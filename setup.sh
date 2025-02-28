5e5b2c96e8d36d6f4d4c7eaf13c22ff961cacf4d
Shin Yamamoto <shin@scenee.com>
2025-02-28 10:41:46 +0900

    setup: add backup function

--- setup.sh
+++ setup.sh
@@ -7,6 +7,28 @@ pushd ~/ >/dev/null
 mkdir -p .local/go
 mkdir -p .local/cargo
 
+# バックアップ用の関数を定義
+backup() {
+    local target=$1
+    if [ -e "$target" ] && [ ! -L "$target" ]; then
+        echo "Backing up $target to ${target}.bak"
+        mv "$target" "${target}.bak"
+    fi
+}
+
+backup .shinit
+backup .bashrc
+backup .zshrc
+backup .zprofile
+backup .profile
+backup .vimrc
+backup .tigrc
+backup .plantuml
+backup .ripgreprc
+backup .gitconfig
+backup .git-completion.bash
+backup .tmux.conf
+
 ln -snf ${DOTFILES}/.shinit
 ln -snf ${DOTFILES}/.bashrc
 ln -snf ${DOTFILES}/.zshrc
@@ -21,14 +43,17 @@ ln -snf ${DOTFILES}/.git-completion.bash
 ln -snf ${DOTFILES}/.tmux.conf .tmux.conf
 
 if [ "$(uname)" = 'Linux' ]; then
-	ln -snf ${DOTFILES}/.tmux.linux.conf .tmux.linux.conf
+    ln -snf ${DOTFILES}/.tmux.linux.conf .tmux.linux.conf
 elif [ "$(uname)" = 'Darwin' ]; then
-	ln -snf ${DOTFILES}/.tmux.mac.conf .tmux.mac.conf
+    ln -snf ${DOTFILES}/.tmux.mac.conf .tmux.mac.conf
 fi
 
 if [ -d .config ]; then
-	ln -snf ${DOTFILES}/fish .config/fish
-	ln -snf ${DOTFILES}/asdf .config/asdf
-	ln -snf ${DOTFILES}/git .config/git
+    backup .config/fish
+    backup .config/asdf
+    backup .config/git
+    ln -snf ${DOTFILES}/fish .config/fish
+    ln -snf ${DOTFILES}/asdf .config/asdf
+    ln -snf ${DOTFILES}/git .config/git
 fi
 
