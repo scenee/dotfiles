b1bf2f1982fb1dc076781f6e76387559caee6e78
Shin Yamamoto <shin@scenee.com>
2025-09-25 12:51:08 +0900

    fish: update add_path.fish

--- .config/fish/conf.d/add_path.fish
+++ .config/fish/conf.d/add_path.fish
@@ -11,6 +11,10 @@ fish_add_path "$HOME/.local/asdf/shims"
 # android
 
 fish_add_path $HOME/Library/Android/sdk/platform-tools
+fish_add_path $HOME/Library/Android/sdk/cmdline-tools/latest/bin
+
+set -gx PATH $PATH $HOME/Library/Android/sdk/build-tools/36.0.0
+set -gx ANDROID_NDK_HOME $HOME/Library/Android/sdk/ndk/27.0.12077973
 
 
 # google-cloud-sdk
